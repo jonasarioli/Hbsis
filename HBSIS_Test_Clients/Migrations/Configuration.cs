898c37240daa988cbec34e3ea60264110c410d5a
Jonas Arioli <jarioli@lenovo.com>
2017-07-11 15:29:43 -0300

    Chnaged seed method.

--- HBSIS_Test_Clients/Migrations/Configuration.cs
+++ HBSIS_Test_Clients/Migrations/Configuration.cs
@@ -7,26 +7,30 @@ using System.Web;
 
 namespace HBSIS_Test_Clients.Migrations
 {
-    internal sealed class Configuration : DbMigrationsConfiguration<DBEntities>
+    internal sealed class Configuration : DbMigrationsConfiguration<ClientRegistryContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            this.AutomaticMigrationDataLossAllowed = true;
+            this.AutomaticMigrationsEnabled = false;
         }
-        protected override void Seed(DBEntities context)
+        protected override void Seed(ClientRegistryContext context)
         {
             //  This method will be called after migrating to the latest version.
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             context.Clientes.AddOrUpdate(
               p => p.Nome,
-              new Clientes { Nome = "Joao", Codigo = "201701", Documento = "224234234234", Telefone="19874569874" },
-              new Clientes { Nome = "Jose", Codigo = "201702", Documento = "224234234234", Telefone = "19874569874" },
-              new Clientes { Nome = "Julia", Codigo = "201703", Documento = "224234234234", Telefone = "19874569874" },
-              new Clientes { Nome = "Julian", Codigo = "201704", Documento = "224234234234", Telefone = "19874569874" },
-              new Clientes { Nome = "Juliana", Codigo = "201705", Documento = "224234234234", Telefone = "19874569874" },
-              new Clientes { Nome = "Jesse", Codigo = "201706", Documento = "224234234234", Telefone = "19874569874" }
+              new Clientes { Id = 1, Nome = "Joao", Codigo = "201701", Documento = "224234234234", Telefone="19874569874" },
+              new Clientes { Id = 2, Nome = "Jose", Codigo = "201702", Documento = "224234234234", Telefone = "19874569874" },
+              new Clientes { Id = 3, Nome = "Julia", Codigo = "201703", Documento = "224234234234", Telefone = "19874569874" },
+              new Clientes { Id = 4, Nome = "Julian", Codigo = "201704", Documento = "224234234234", Telefone = "19874569874" },
+              new Clientes { Id = 5, Nome = "Juliana", Codigo = "201705", Documento = "224234234234", Telefone = "19874569874" },
+              new Clientes { Id = 6, Nome = "Jesse", Codigo = "201706", Documento = "224234234234", Telefone = "19874569874" }
             );
+
+            base.Seed(context);
+            context.SaveChanges();
         }
     }
 }
